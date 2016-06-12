2069751bb4b5dd5362e1478943ad5bd13afe4453
kieykouch <ki.ey.kouch@gmail.com>
2016-06-12 11:33:10 -0700

    cap first letter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :capitalize_names
+
+  def capitalize_names
+    self.first_name = first_name.camelcase
+    self.last_name = last_name.camelcase
+  end
 end
