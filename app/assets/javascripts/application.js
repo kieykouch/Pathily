9e95db6232e9eae97bcd5584854f6eff68cbb05f
kieykouch <ki.ey.kouch@gmail.com>
2016-08-11 23:06:10 -0700

    notify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,36 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 
+
+$(function() {
+
+  $('#job_alert_tag2').click(function(){ 
+
+  	console.log("fuck you");
+
+	function validateEmail(email) {
+	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return re.test(email);
+	}
+
+
+	var value = $(".form-control")[5];
+	var value2 = $(value).val();
+	console.log(value);
+	console.log(value2);
+
+	if (value2 != "" && validateEmail(value2))
+	{                                  
+	    $('#job_alert_tag2').notify 
+	    ("Summited the email", "success", { position:"right" });
+	}
+	else{
+		$('#job_alert_tag2').notify 
+	    ("Please check your email", "error", { position:"right" });
+	}
+	});
+});
+
 ///= require bootstrap-sprockets
 //= require bootstrap
 //= require_tree .
