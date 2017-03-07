09fce8c3f7636a29f3901b0e2684a5c84ac90cea
Ki Kouch <ki.ey.kouch@gmail.com>
2017-03-06 23:06:15 -0800

    Update omniauth_callbacks_controller.rb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,20 +3,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	include HTTParty
 
     def linkedin
-    	print "*****\n"
-    	print request.env["omniauth.auth"]
-    	print "\n********"
-	 	user = User.from_omniauth(request.env["omniauth.auth"])
-	 	if user.persisted?
-	 		flash.notice = "Signed In"
-	 		sign_in_and_redirect user
-	 	else
-	 		session["devise.user_attributes"]
-	 		redirect_to new_user_registration_path
-	 	end
-	 end
+	    
+	user = User.from_omniauth(request.env["omniauth.auth"])
+	if user.persisted?
+		flash.notice = "Signed In"
+		sign_in_and_redirect user
+	else
+		session["devise.user_attributes"]
+		redirect_to new_user_registration_path
+	end
+	end
 
-	 def facebook
+	def facebook
     	user = User.from_omniauth_facebook(request.env["omniauth.auth"])
 	 	if user.persisted?
 	 		flash.notice = "Signed In"
@@ -28,16 +26,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	 end
 
 	 def google_oauth2
-	 	
-
-	 	#token = request.env["omniauth.auth"]["credentials"]["token"]
-	 	#response = HTTParty.get("https://www.google.com/m8/feeds/contacts/default/thin?access_token="+token+"&alt=json")
-	 	#contacts = JSON.parse(response.body)
-	 	#contacts = contacts["feed"]["entry"]
-	 	#contacts1 = contacts.select{|p| p["gd$email"]}
-	 	#contacts1 = contacts1.collect{|p| {name: p["title"]["$t"], email: p["gd$email"][0]["address"]} }
-	 	
-    	user = User.from_omniauth_gmail(request.env["omniauth.auth"])
+    	 user = User.from_omniauth_gmail(request.env["omniauth.auth"])
 	 	if user.persisted?
 	 		flash.notice = "Signed In"
 	 		sign_in_and_redirect user
